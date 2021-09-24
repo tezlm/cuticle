75751d1431ec7a5c81245f2575768ff28402e28b
sample-text-here <56274831+sample-text-here@users.noreply.github.com>
2021-09-24 16:28:16 -0700

    alt = 5x speed

---
+++ README.md
@@ -0,0 +1,9 @@
+# chromiutil
+
+utility scripts for chrome. current features:
+
+- <key>alt-p</key> to put videos in pip
+	- if multiple videos are found, you can pick which one to put in pip
+	- press again to unpip
+- hold alt to temporarily speed up videos 5x
+
--- background.js
+++ background.js
@@ -1,19 +1,29 @@
 console.log("hi");
 
-chrome.commands.onCommand.addListener((command) => {
+chrome.commands.onCommand.addListener(async (command) => {
 	switch(command) {
 		case "pip":
-			run("pip.js");
+			run(await getTab(), "pip.js");
 			break;
 	}
 });
 
-async function run(file) {
+chrome.tabs.onUpdated.addListener((tab, { status }) => {
+	if(status !== "complete") return;
+    run(tab, "altspeed.js");
+});
+
+async function getTab() {
 	const queryOptions = { active: true, currentWindow: true };
   	const [tab] = await chrome.tabs.query(queryOptions);
 	if(!tab) return;
+	return tab.id;
+}
+
+async function run(tabId, file) {
+	console.log(tabId);
     chrome.scripting.executeScript({
-		target: { tabId: tab.id},
+		target: { tabId },
 		files: [file],
 	});
 }
